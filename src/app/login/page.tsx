51177b50997c01ea995c01731c04fd9318b40f97
hyuna <hyuna_0525@dsm.hs.kr>
2024-03-28 16:03:23 +0900

    fix : 로그인 페이지 반응형

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -69,59 +69,57 @@ const Login: NextPage = () => {
   };
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className=" font-sans flex justify-center items-center gap-6 bg-primary-1200 p-sign h-lvh">
-        <div className=" flex flex-col gap-10 px-44 bg-white rounded-xl h-  p-44">
-          <div className=" font-sans text-heading5 text-primary-100">
-            로그인
-          </div>
-          <div className=" flex flex-col items-center gap-29">
-            <div className=" flex flex-col items-center gap-8">
+    <div className=" font-sans flex justify-center items-center gap-6 bg-primary-1200 h-lvh p-5">
+      <div className=" flex flex-col gap-10 bg-white rounded-xl h-5/6 min-w-96 w-2/5 justify-center items-center ">
+        <div className=" font-sans text-heading5 text-primary-100">로그인</div>
+        <div className=" flex flex-col items-center gap-29 w-8/12">
+          <div className=" flex flex-col items-center gap-8 w-full">
+            <div className="flex flex-col w-full">
               <Input
                 type="text"
-                width="92"
                 label="아이디"
                 placeholder="아이디"
+                width="full"
                 onChange={handleChange}
                 value={data.admin_id}
                 onKeyDown={handleKeyDown}
                 name="admin_id"
               />
-              <div className="flex flex-col gap-6">
-                <Input
-                  type="password"
-                  width="92"
-                  label="비밀번호"
-                  placeholder="비밀번호"
-                  onChange={handleChange}
-                  onKeyDown={handleKeyDown}
-                  name="password"
-                  value={data.password}
-                />
-              </div>
             </div>
-            <Button
-              colorType={`${BtnColor()}`}
-              buttonSize="full"
-              onClick={onClickBtn}
-            >
-              로그인
-            </Button>
+            <div className="flex flex-col w-full">
+              <Input
+                type="password"
+                width="full"
+                label="비밀번호"
+                placeholder="비밀번호"
+                onChange={handleChange}
+                onKeyDown={handleKeyDown}
+                name="password"
+                value={data.password}
+              />
+            </div>
           </div>
+          <Button
+            colorType={`${BtnColor()}`}
+            buttonSize="full"
+            onClick={onClickBtn}
+          >
+            로그인
+          </Button>
         </div>
-        <div className=" w-100 h- rounded-xl bg-sideImg">
-          <div className=" rounded-xl py-12 px-13 h-full bg-primary-1000 backdrop-blur-sm bg-opacity-30 bg-blur-md">
-            <div className="font-sans flex gap-3.5 text-heading6-M text-white">
-              {Picklogo && <img src={Picklogo.src} width={110} height={60} />}
-            </div>
-            <div className=" text-heading1 -space-y-5">
-              <div className=" text-white">Welcome</div>
-              <div className=" text-primary-500">PiCK</div>
-            </div>
+      </div>
+      <div className=" w-100 h-5/6 rounded-xl bg-sideImg">
+        <div className=" rounded-xl py-12 px-13 h-full bg-primary-1000 backdrop-blur-sm bg-opacity-30 bg-blur-md">
+          <div className="font-sans flex gap-3.5 text-heading6-M text-white w-3/5">
+            {Picklogo && <img src={Picklogo.src} width={110} height={60} />}
+          </div>
+          <div className=" text-heading1 -space-y-5">
+            <div className=" text-white">Welcome</div>
+            <div className=" text-primary-500">PiCK</div>
           </div>
         </div>
       </div>
-    </QueryClientProvider>
+    </div>
   );
 };
 
