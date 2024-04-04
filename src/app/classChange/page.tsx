1e083dd6fc85c2ed789fcdace8925aabe643741c
hyuna <hyuna_0525@dsm.hs.kr>
2024-04-04 23:16:10 +0900

    add : 교실 이동

--- src/app/classChange/page.tsx
+++ src/app/classChange/page.tsx
@@ -1,96 +1,132 @@
 "use client";
-import React, { useEffect } from "react";
-import DoubleTab from "../components/common/tab/page";
 import { getFullToday } from "@/utils/date";
-import { useState } from "react";
-import Dropdown from "../components/common/dropdown";
-import ChangeClass from "../components/common/list/changeClass/page";
-import { ChangeClassList, GetFloor } from "@/apis/changeClass";
-import { getStudentString } from "@/utils/until";
 import { BackGround } from "../components/common/background";
+import { AcceptClass, AcceptClassChange } from "@/apis/changeClass";
+import Dropdown from "../components/common/dropdown";
+import { useEffect, useState } from "react";
+import Button from "../components/common/Button";
+import { useRouter } from "next/navigation";
+import ChangeClass from "../components/common/list/changeClass/page";
+import { getStudentString } from "@/utils/until";
+import Modal from "../components/common/modal/page";
 
-interface changeClass {
+interface FloorClass {
+  id: string;
   class_num: number;
   classroom_name: string;
-  floor: number;
+  end_period: number;
   grade: number;
-  id: string;
+  move: string;
   num: number;
-  user_id: string;
+  start_period: number;
   username: string;
 }
 
-const OutList = () => {
-  const [selectedTab, setSelectedTab] = useState<boolean>(true);
-  const [selectedGrade, setSelectedGrade] = useState<number>(1);
-  const [selectedClass, setSelectedClass] = useState<number>(1);
-  const { mutate: changelistMutate } = ChangeClassList();
-  const [floorData, setFloorData] = useState<changeClass[]>([]);
-  const { mutate: changelistFloorMutate } = GetFloor();
-  const [changelist, setChangelist] = useState<changeClass[]>([]);
+const ClassChange = () => {
   const [selectedFloor, setSelectedFloor] = useState<number>(2);
+  const [data, setData] = useState<FloorClass[]>([]);
+  const [accept, setAccept] = useState<boolean>(false);
+  const [refuse, setRefuse] = useState<boolean>(false);
+  const [selectedStudents, setSelectedStudents] = useState<string[]>([]);
+  const [selectedStudentName, setSelectedStudentName] = useState<string[]>([]);
 
-  const onClickTab = (tab: boolean) => {
-    setSelectedTab(tab);
+  const { mutate: AccpetMutate } = AcceptClassChange();
+  const { mutate: AccpetList } = AcceptClass();
+
+  const Accept = async () => {
+    setAccept(true);
   };
 
-  const handleGradeChange = (selectedOption: number) => {
-    setSelectedGrade(selectedOption);
+  const Refuse = () => {
+    setRefuse(true);
   };
 
-  const handleClassChange = (selectedOption: number) => {
-    setSelectedClass(selectedOption);
+  const handleAcceptListClick = (id: string, name: string) => {
+    const isStudentSelected = selectedStudents.includes(id);
+    if (isStudentSelected) {
+      setSelectedStudents((prevSelectedStudents) =>
+        prevSelectedStudents.filter((selectedStudent) => selectedStudent !== id)
+      );
+      setSelectedStudentName((prevSelectedStudentName) =>
+        prevSelectedStudentName.filter(
+          (selectedStudentName) => selectedStudentName !== name
+        )
+      );
+    } else {
+      setSelectedStudents((prevSelectedStudents) => [
+        ...prevSelectedStudents,
+        id,
+      ]);
+      setSelectedStudentName((prevSelectedStudentName) => [
+        ...prevSelectedStudentName,
+        name,
+      ]);
+    }
   };
 
-  const handleFloorChange = (selectedOption: number) => {
-    setSelectedFloor(selectedOption);
-  };
-
-  useEffect(() => {
-    changeClassData();
-  }, [selectedGrade, selectedClass]);
-
-  useEffect(() => {
-    ChangeClassDataFloor();
-  }, [selectedFloor]);
-
-  useEffect(() => {
-    changeClassData();
-  }, []);
-
-  const ChangeClassDataFloor = async () => {
+  const Accpet = async () => {
     try {
-      if (selectedFloor) {
-        await changelistFloorMutate(
-          { floor: selectedFloor },
-          {
-            onSuccess: (data) => {
-              setFloorData(data);
-            },
-            onError: (error) => {
-              console.log(error);
-            },
-          }
-        );
-      }
+      await AccpetMutate(
+        { floor: selectedFloor },
+        {
+          onSuccess: (data) => {
+            setData(data);
+          },
+          onError: (error) => {
+            alert(error.name);
+          },
+        }
+      );
     } catch (error) {
       console.log(error);
     }
   };
 
-  const changeClassData = async () => {
+  const nav = useRouter();
+
+  useEffect(() => {
+    Accpet();
+  }, [selectedFloor]);
+
+  const handleFloorChange = (selectedOption: number) => {
+    setSelectedFloor(selectedOption);
+  };
+
+  const closeModal = () => {
+    setRefuse(false);
+  };
+
+  const AcceptrCancel = () => {
+    setAccept(false);
+  };
+
+  const confirmReturn = async () => {
     try {
-      if (selectedClass && selectedGrade) {
-        await changelistMutate(
-          { grade: selectedGrade, class: selectedClass },
-          {
-            onSuccess: (data) => {
-              console.log(data);
-              setChangelist(data);
-            },
-          }
-        );
-      }
+      await AccpetList(
+        { status: "NO", id: selectedStudents },
+        {
+          onSuccess: () => {
+            location.reload();
+            alert("교실이동이 거절되었습니다.");
+          },
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const Acceptconfirm = async () => {
+    try {
+      await AccpetList(
+        { status: "OK", id: selectedStudents },
+        {
+          onSuccess: () => {
+            location.reload();
+            alert("교실이동이 수락되었습니다");
+          },
+        }
+      );
     } catch (error) {
       console.log(error);
     }
@@ -98,50 +134,94 @@ const OutList = () => {
 
   return (
     <BackGround
-      subTitle="교실 이동"
+      subTitle="교실 이동 수락"
       secondTitle={getFullToday()}
       linkChildren="교실 이동"
       DropChildren={
         <>
-          <DoubleTab
-            firstChildren="반 별로 보기"
-            secondChildren="층 별로 보기"
-            onClick={onClickTab}
-          />
-          <div className="flex gap-5">
-            {selectedTab ? (
-              <>
-                <Dropdown type="grade" onChange={handleGradeChange} />
-                <Dropdown type="class" onChange={handleClassChange} />
-              </>
-            ) : (
-              <Dropdown type="floor" onChange={handleFloorChange} />
-            )}
+          <div className="flex gap-5 items-center">
+            <Button
+              colorType="ghost"
+              buttonSize="small"
+              onClick={() => {
+                nav.push("/classChange/ok");
+              }}
+            >
+              교실 이동 보기
+            </Button>
+            <Dropdown type="floor" onChange={handleFloorChange} />
           </div>
         </>
       }
     >
-      <div className="flex flex-wrap gap-5 justify-between w-full">
-        {selectedTab
-          ? changelist?.map((item, index) => (
-              <ChangeClass
-                key={index}
-                prevClass={`${item.grade}-${item.class_num}`}
-                nextClass={item.classroom_name}
-                student={getStudentString(item)}
-              />
-            ))
-          : floorData?.map((item, index) => (
-              <ChangeClass
-                key={index}
-                prevClass={`${item.grade}-${item.class_num}`}
-                nextClass={item.classroom_name}
-                student={getStudentString(item)}
-              />
-            ))}
+      {data?.map((item, index) => (
+        <ChangeClass
+          type="accept"
+          key={index}
+          onClick={() => handleAcceptListClick(item.id, item.username)}
+          prevClass={`${item.grade}-${item.class_num}`}
+          nextClass={`${item.classroom_name}`}
+          student={getStudentString(item)}
+        />
+      ))}
+      <div className=" flex gap-5 w-full justify-end">
+        <Button
+          colorType="ghost"
+          buttonSize="medium"
+          onClick={() => {
+            Refuse();
+          }}
+        >
+          거절하기
+        </Button>
+        <Button
+          colorType="primary"
+          buttonSize="medium"
+          onClick={() => {
+            Accept();
+          }}
+        >
+          수락하기
+        </Button>
       </div>
+      {refuse && (
+        <Modal
+          heading1={`${
+            selectedStudentName.length > 1
+              ? `${selectedStudentName[0]} 학생 외 ${
+                  selectedStudentName.length - 1
+                }명`
+              : selectedStudentName.length === 1
+              ? `${selectedStudentName[0]} 학생`
+              : ""
+          }`}
+          heading2={`교실이동을 거절하시겠습니까?`}
+          type="error"
+          buttonMessage="거절"
+          onCancel={closeModal}
+          onConfirm={confirmReturn}
+        />
+      )}
+      {accept && (
+        <Modal
+          heading1={`${
+            selectedStudentName.length > 1
+              ? `${selectedStudentName[0]} 학생 외 ${
+                  selectedStudentName.length - 1
+                }명`
+              : selectedStudentName.length === 1
+              ? `${selectedStudentName[0]} 학생`
+              : ""
+          }`}
+          heading2={`교실이동을 수락하시겠습니까?`}
+          type="button"
+          buttonMessage="수락"
+          onCancel={AcceptrCancel}
+          onConfirm={Acceptconfirm}
+        />
+      )}
     </BackGround>
   );
 };
 
-export default OutList;
+export default ClassChange;
