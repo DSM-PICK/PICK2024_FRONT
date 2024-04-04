008e9e3b2bc7b1432a7f869bb40e729503c33ff7
heedda <heedda.kollim@gmail.com>
2024-04-04 23:24:03 +0900

    방과후 신청 post 완성

--- src/app/afterManage/page.tsx
+++ src/app/afterManage/page.tsx
@@ -68,195 +68,214 @@ const AfterManage = () => {
     setModal(false);
   };
   const [data, setData] = useState<string[]>(() => {
-    if(typeof window !== "undefined") { const localData = localStorage.getItem("students")
-
-    return localData ? JSON.parse(localData) : [];
-  };
+    if (typeof window !== "undefined") {
+      const localData = localStorage.getItem("students");
+      return localData ? JSON.parse(localData) : [];
+    }
   });
 
   const handleModalConfirm = async () => {
+    setData(() => {
+      if (typeof window !== "undefined") {
+        const localData = localStorage.getItem("students");
+        return localData ? JSON.parse(localData) : [];
+      }
+      return [];
+    });
+
+    const updatedData = data.map((item) => {
+      const [studentNum] = item.split(" ");
+      return {
+        student_num: studentNum,
+      };
+    });
+    
+    console.log(data);
     try {
-      await postStudents(data);
+      await postStudents(updatedData);
+      setModal(false);
     } catch (error) {
       console.log(error);
     }
+  };
 
-    const dataFormChange = () => {};
+  // const dataFormChange = () => {
+  //   const updatedData = data.map((item) => {
+  //     const [studentNum] = item.split(" ");
+  //     return {
+  //       student_num: studentNum,
+  //     };
+  //   });
+  //   console.log(updatedData);
 
-    const handleSaveModalCancel = () => {
-      setSaveModal(false);
-    };
+  //   setData(updatedData);
+  // };
 
-    const handleSaveModalConfirm = () => {
-      setSaveModal(false);
-    };
+  const handleSaveModalCancel = () => {
+    setSaveModal(false);
+  };
 
-    const threeStyle = " bg-white text-label1 rounded-lg py-3 px-25";
-    
-    const onClickBtn = () => {
-      router.push("/main");
-    };
+  const handleSaveModalConfirm = () => {
+    setSaveModal(false);
+  };
 
-    return (
-      <BackGround
-        secondTitle={
-          <AfterTab
-            firstChildren="전공동아리"
-            secondChildren="방과후"
-            onClick={onClickChange}
-          ></AfterTab>
-        }
-        subTitle="방과후 관리"
-        linkChildren="방과후 관리"
-        DropChildren={
-          <>
-            {change ? (
-              <div className="flex items-center gap-5 flex-wrap justify-end">
-                {edit ? (
-                  <Button
-                    colorType="ghost"
-                    buttonSize="small"
-                    onClick={onClickSave}
-                  >
-                    출결 저장하기
-                  </Button>
-                ) : (
-                  <Button
-                    colorType="ghost"
-                    buttonSize="small"
-                    onClick={onClickEdit}
-                  >
-                    출결 체크하기
-                  </Button>
-                )}
-                <div className=" flex gap-2">
-                  <Dropdown type="floor" />
-                  <Dropdown type="club" />
-                </div>
-              </div>
-            ) : (
-              <div className=" flex gap-4">
+  const threeStyle = " bg-white text-label1 rounded-lg py-3 px-25";
+
+  const onClickBtn = () => {
+    router.push("/main");
+  };
+
+  return (
+    <BackGround
+      secondTitle={
+        <AfterTab
+          firstChildren="전공동아리"
+          secondChildren="방과후"
+          onClick={onClickChange}
+        ></AfterTab>
+      }
+      subTitle="방과후 관리"
+      linkChildren="방과후 관리"
+      DropChildren={
+        <>
+          {change ? (
+            <div className="flex items-center gap-5 flex-wrap justify-end">
+              {edit ? (
                 <Button
                   colorType="ghost"
                   buttonSize="small"
-                  onClick={onClickAdd}
+                  onClick={onClickSave}
                 >
-                  인원추가하기
+                  출결 저장하기
                 </Button>
-                {edit ? (
-                  <Button
-                    colorType="ghost"
-                    buttonSize="small"
-                    onClick={onClickSave}
-                  >
-                    출결저장하기
-                  </Button>
-                ) : (
-                  <Button
-                    colorType="ghost"
-                    buttonSize="small"
-                    onClick={onClickEdit}
-                  >
-                    출결 체크하기
-                  </Button>
-                )}
+              ) : (
+                <Button
+                  colorType="ghost"
+                  buttonSize="small"
+                  onClick={onClickEdit}
+                >
+                  출결 체크하기
+                </Button>
+              )}
+              <div className=" flex gap-2">
+                <Dropdown type="floor" />
+                <Dropdown type="club" />
               </div>
-            )}
-          </>
-        }
-      >
-        <>
-          {change ? (
-            <div className=" w-full h-full text-heading5 flex flex-col justify-center items-center gap-4">
-              아직 개발중인 페이지입니다
-              <Button
-                colorType="primary"
-                buttonSize="extraLarge"
-                onClick={onClickBtn}
-              >
-                홈으로 돌아가기
-              </Button>
             </div>
           ) : (
-            <>
+            <div className=" flex gap-4">
+              <Button colorType="ghost" buttonSize="small" onClick={onClickAdd}>
+                인원추가하기
+              </Button>
               {edit ? (
-                <div className=" flex flex-col gap-8">
-                  <div className=" flex gap-20">
-                    <div className=" text-heading5 justify-center flex text-primary-100 w-29">
-                      창조실
-                    </div>
-                    <div className="flex gap-11">
-                      <div className={threeStyle}>8교시</div>
-                      <div className={threeStyle}>9교시</div>
-                      <div className={threeStyle}>10교시</div>
-                    </div>
-                  </div>
-                  <div className=" flex gap-13">
-                    <div className=" flex flex-col gap-6 w-27%">
-                      {datalist?.map((item, index) => (
-                        <div
-                          className="flex w-max bg-white py-4 px-6 rounded-lg text-label1"
-                          key={index}
-                        >
-                          {item.name}
-                        </div>
-                      ))}
-                    </div>
-                    <div className=" flex gap-x-11 gap-y-6 flex-wrap content-start">
-                      {edit ? (
-                        <>
-                          {/* {datalist?.map((item, index) => {
-                          <AfterCheck state={"무단"} key={index} day={3} />;
-                        })} */}
-                          {datalist?.map((item, index) => {
-                            <AfterCheck state={"무단"} key={index} day={3} />;
-                          })}
-                        </>
-                      ) : (
-                        <></>
-                      )}
-                    </div>
-                  </div>
-                </div>
+                <Button
+                  colorType="ghost"
+                  buttonSize="small"
+                  onClick={onClickSave}
+                >
+                  출결저장하기
+                </Button>
               ) : (
-                <div className=" flex gap-8 flex-col w-full">
-                  <div className=" text-heading5 text-primary-100">창조실</div>
-                  <div className=" flex gap-x-13 gap-y-5 flex-wrap w-full">
-                    {datalist?.map((item, index) => (
-                      <AfterDelete
-                        student={item.name}
-                        key={index}
-                        id={item.id}
-                      />
-                    ))}
-                  </div>
-                </div>
+                <Button
+                  colorType="ghost"
+                  buttonSize="small"
+                  onClick={onClickEdit}
+                >
+                  출결 체크하기
+                </Button>
               )}
-              {modal && (
-                <Modal
-                  type="add"
-                  heading1="창조실 인원추가"
-                  buttonMessage="추가"
-                  onCancel={handleModalCancel}
-                  onConfirm={handleModalConfirm}
-                />
-              )}
-              {saveModal && (
-                <Modal
-                  type="button"
-                  heading1={`${datalist?.length}외 1명의`}
-                  heading2="변경된 상태를 저장하시겠습니까?"
-                  buttonMessage="확인"
-                  onCancel={handleSaveModalCancel}
-                  onConfirm={handleSaveModalConfirm}
-                />
-              )}
-            </>
+            </div>
           )}
         </>
-      </BackGround>
-    );
-  };
+      }
+    >
+      <>
+        {change ? (
+          <div className=" w-full h-full text-heading5 flex flex-col justify-center items-center gap-4">
+            아직 개발중인 페이지입니다
+            <Button
+              colorType="primary"
+              buttonSize="extraLarge"
+              onClick={onClickBtn}
+            >
+              홈으로 돌아가기
+            </Button>
+          </div>
+        ) : (
+          <>
+            {edit ? (
+              <div className=" flex flex-col gap-8">
+                <div className=" flex gap-20">
+                  <div className=" text-heading5 justify-center flex text-primary-100 w-29">
+                    창조실
+                  </div>
+                  <div className="flex gap-11">
+                    <div className={threeStyle}>8교시</div>
+                    <div className={threeStyle}>9교시</div>
+                    <div className={threeStyle}>10교시</div>
+                  </div>
+                </div>
+                <div className=" flex gap-13">
+                  <div className=" flex flex-col gap-6 w-27%">
+                    {datalist?.map((item, index) => (
+                      <div
+                        className="flex w-max bg-white py-4 px-6 rounded-lg text-label1"
+                        key={index}
+                      >
+                        {item.name}
+                      </div>
+                    ))}
+                  </div>
+                  <div className=" flex gap-x-11 gap-y-6 flex-wrap content-start">
+                    {edit ? (
+                      <>
+                        {/* {datalist?.map((item, index) => {
+                          <AfterCheck state={"무단"} key={index} day={3} />;
+                        })} */}
+                        {datalist?.map((item, index) => {
+                          <AfterCheck state={"무단"} key={index} day={3} />;
+                        })}
+                      </>
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className=" flex gap-8 flex-col w-full">
+                <div className=" text-heading5 text-primary-100">창조실</div>
+                <div className=" flex gap-x-13 gap-y-5 flex-wrap w-full">
+                  {datalist?.map((item, index) => (
+                    <AfterDelete student={item.name} key={index} id={item.id} />
+                  ))}
+                </div>
+              </div>
+            )}
+            {modal && (
+              <Modal
+                type="add"
+                heading1="창조실 인원추가"
+                buttonMessage="추가"
+                onCancel={handleModalCancel}
+                onConfirm={handleModalConfirm}
+              />
+            )}
+            {saveModal && (
+              <Modal
+                type="button"
+                heading1={`${datalist?.length}외 1명의`}
+                heading2="변경된 상태를 저장하시겠습니까?"
+                buttonMessage="확인"
+                onCancel={handleSaveModalCancel}
+                onConfirm={handleSaveModalConfirm}
+              />
+            )}
+          </>
+        )}
+      </>
+    </BackGround>
+  );
 };
 
 export default AfterManage;
