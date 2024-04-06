781a57ac00510dfe0afbccf829c4bde8dc16c1fe
hyuna <hyuna_0525@dsm.hs.kr>
2024-04-06 11:08:56 +0900

    test

--- src/app/components/common/calendar/schedule/page.tsx
+++ src/app/components/common/calendar/schedule/page.tsx
@@ -6,6 +6,7 @@ import Calendar from "react-calendar";
 import { GetSchdule } from "@/apis/schedule";
 import ScheduleFix from "./fix";
 import PostSchedule from "../../modal/schedule/page";
+import "moment/locale/en-gb";
 
 interface CalendarProps {
   onClick: (date: Date) => void;
@@ -26,6 +27,8 @@ const ScheduleCalendar: React.FC<CalendarProps> = ({ onClick, onChange }) => {
   const { mutate: scheduleMutate } = GetSchdule();
 
   const currentYear = new Date().getFullYear();
+
+  moment.locale("en");
 
   const scheduleData = async (selectDate: Date | null) => {
     const formattedDate = moment(selectDate).format("MMMM");
@@ -78,7 +81,7 @@ const ScheduleCalendar: React.FC<CalendarProps> = ({ onClick, onChange }) => {
         next2Label={null}
         calendarType="gregory"
         formatDay={(locale, date) =>
-          date.toLocaleString("en", { day: "2-digit" })
+          date.toLocaleString("en", { day: "numeric" })
         }
         tileContent={({ date }) => {
           const formattedDate = moment(date).format("M-D");
