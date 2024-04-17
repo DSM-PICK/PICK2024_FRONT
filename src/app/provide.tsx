e5405530c5d7c3c3710a26ec93fb090a40f14721
hyuna <hyuna_0525@dsm.hs.kr>
2024-04-17 17:11:20 +0900

    fix : 토큰 관련 수정

--- src/app/provide.tsx
+++ src/app/provide.tsx
@@ -1,13 +1,27 @@
 "use client";
+import { cookie } from "@/utils/auth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
-interface clidrenProp {
+interface ProviderProps {
   children: React.ReactNode;
 }
 
-export const Provider = ({ children }: clidrenProp) => {
+export const Provider: React.FC<ProviderProps> = ({ children }) => {
+  const router = useRouter();
+
+  const accessToken = cookie.get("access_token");
+
+  useEffect(() => {
+    if (!accessToken) {
+      alert("로그인 후 이용해 주세요");
+      router.push("/login");
+    }
+  }, []);
+
   const queryClient = new QueryClient();
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
