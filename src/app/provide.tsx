f4da7c30a713732391051d3dd77e857d916fbf36
hyuna <hyuna_0525@dsm.hs.kr>
2024-05-31 22:48:18 +0900

    fix : 빌드오류

--- src/app/provide.tsx
+++ src/app/provide.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { cookie } from "@/utils/auth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
@@ -8,17 +7,6 @@ interface ProviderProps {
 }
 
 export const Provider: React.FC<ProviderProps> = ({ children }) => {
-  const router = useRouter();
-
-  const accessToken = cookie.get("access_token");
-
-  useEffect(() => {
-    if (!accessToken) {
-      router.push("/login");
-      alert("로그인 후 이용해 주세요");
-    }
-  }, []);
-
   const queryClient = new QueryClient();
 
   return (
