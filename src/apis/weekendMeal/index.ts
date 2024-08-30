962e026afe15118f1568e8c557ac5030bcbe8085
hyuna <hyuna_0525@dsm.hs.kr>
2024-08-30 09:11:12 +0900

    add :: 반별 엑셀출력 api

--- src/apis/weekendMeal/index.ts
+++ src/apis/weekendMeal/index.ts
@@ -1,6 +1,7 @@
 import apiError from "@/hook/apiError";
 import { instance } from "..";
 import { useMutation, useQuery } from "@tanstack/react-query";
+import { link } from "fs";
 
 interface ChangeStateParams {
   status: "OK" | "NO";
@@ -46,6 +47,35 @@ export const Printexcel = () => {
   };
 
   return { downloadExcel };
+};
+
+export const useClassWeekendMealExcel = () => {
+  const usedownloadClassExcel = async (grade: number, class_num: number) => {
+    try {
+      const { data } = await instance.get(
+        `/weekend-meal/excel/grade?grade=${grade}&class_num=${class_num}`,
+        {
+          responseType: "blob",
+        }
+      );
+
+      const url = window.URL.createObjectURL(new Blob([data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute(
+        "download",
+        `${grade}학년 ${class_num}반 주말급식.xlsx`
+      );
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } catch (error) {
+      console.log(error);
+      alert("잠시후 다시 시도해주세요");
+    }
+  };
+
+  return { usedownloadClassExcel };
 };
 
 export const ChangeState = () => {
